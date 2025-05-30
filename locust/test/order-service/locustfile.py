e8783b2712c9266a2d5509463649509a238460b3
Camilo Andres Bueno De La Pava <camilobueno05@gmail.com>
2025-05-29 21:37:41 -0500

    locust done

---
+++ locust/test/order-service/locustfile.py
@@ -0,0 +1,35 @@
+from locust import HttpUser, task, between
+import random
+
+class OrderServiceUser(HttpUser):
+    wait_time = between(1, 3)
+    order_ids = [1, 2, 3, 4]  # Asegúrate de que existan
+
+    @task
+    def getAllOrders(self):
+        path = "/order-service/api/orders"
+        with self.client.get(path, catch_response=True, name="/api/orders") as response:
+            if response.status_code >= 200 and response.status_code < 300:
+                response.success()
+            else:
+                response.failure(f"Unexpected status code: {response.status_code} | Body: {response.text}")
+
+    @task
+    def getOrderById(self):
+        order_id = random.choice(self.order_ids)
+        path = f"/order-service/api/orders/{order_id}"
+        with self.client.get(path, catch_response=True, name="/api/orders/{id}") as response:
+            if response.status_code >= 200 and response.status_code < 300:
+                response.success()
+            else:
+                response.failure(f"Unexpected status code: {response.status_code} | Body: {response.text}")
+
+    @task
+    def deleteOrderById(self):
+        order_id = random.choice(self.order_ids)
+        path = f"/order-service/api/orders/{order_id}"
+        with self.client.delete(path, catch_response=True, name="/api/orders/{id} [DELETE]") as response:
+            if response.status_code >= 200 and response.status_code < 300:
+                response.success()
+            else:
+                response.failure(f"Failed to delete order {order_id}: {response.status_code} | {response.text}")
---
+++ locust/test/payment-service/locustfile.py
@@ -0,0 +1,26 @@
+from locust import HttpUser, task, between
+import random
+
+class PaymentServiceUser(HttpUser):
+    wait_time = between(1, 3)
+    payment_ids = [1, 2, 3]
+    order_ids = [1, 2, 3, 4]
+
+    @task
+    def getAllPayments(self):
+        path = "/payment-service/api/payments"
+        with self.client.get(path, catch_response=True, name="/api/payments") as response:
+            if response.status_code >= 200 and response.status_code < 300:
+                response.success()
+            else:
+                response.failure(f"Unexpected status code: {response.status_code}")
+
+    @task
+        def getPaymentById(self):
+            payment_id = random.choice(self.payment_ids)
+            path = f"/payment-service/api/payments/{payment_id}"
+            with self.client.get(path, catch_response=True, name="/api/payments/{id}") as response:
+                if response.status_code >= 200 and response.status_code < 300:
+                    response.success()
+                else:
+                    response.failure(f"Unexpected status code: {response.status_code}")
