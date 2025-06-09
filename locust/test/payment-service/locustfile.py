396eaf89fb0fe6897c5916ebf0e9b93878e8e906
Camilo Andres Bueno De La Pava <camilobueno05@gmail.com>
2025-06-08 22:07:46 -0500

    add print for debug

--- locust/test/payment-service/locustfile.py
+++ locust/test/payment-service/locustfile.py
@@ -4,7 +4,6 @@ import random
 class PaymentServiceUser(HttpUser):
     wait_time = between(1, 3)
     payment_ids = [1, 2, 3]
-    order_ids = [1, 2, 3]
 
     @task
     def getAllPayments(self):
@@ -12,6 +11,7 @@ class PaymentServiceUser(HttpUser):
         with self.client.get(path, catch_response=True, name="/api/payments") as response:
             if response.status_code >= 200 and response.status_code < 300:
                 response.success()
+                print(f"Response: {response.text}")
             else:
                 response.failure(f"Unexpected status code: {response.status_code}")
 
