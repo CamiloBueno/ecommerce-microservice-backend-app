4a6b46e8a7a1115680461283bb20a7ddc303ea2b
Camilo Andres Bueno De La Pava <camilobueno05@gmail.com>
2025-06-08 22:26:11 -0500

    add test for delete order and remove test for get all payments

--- locust/test/order-service/locustfile.py
+++ locust/test/order-service/locustfile.py
@@ -23,14 +23,13 @@ class OrderServiceUser(HttpUser):
                 response.success()
             else:
                 response.failure(f"Unexpected status code: {response.status_code} | Body: {response.text}")
-'''
+
     @task
     def deleteOrderById(self):
         order_id = random.choice(self.order_ids)
-        path = f"/order-service/api/orders/{order_id}"
+        path = f"/order-service/api/orders/4"
         with self.client.delete(path, catch_response=True, name="/api/orders/{id} [DELETE]") as response:
             if response.status_code >= 200 and response.status_code < 300:
                 response.success()
             else:
                 response.failure(f"Failed to delete order {order_id}: {response.status_code} | {response.text}")
-'''
